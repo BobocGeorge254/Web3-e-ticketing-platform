c61d9ae62d85e84e44db0cdfa8aa035a133adced
GeorgeBoboc254 <ghitzaboboc@gmail.com>
2024-05-17 18:07:35 +0300

    added mapping type

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,13 +2,11 @@
 const hre = require("hardhat");
 
 async function main() {
-    console.log('1')
+
     const Flight = await hre.ethers.getContractFactory("Flight");
-    console.log('2')
     const flight = await Flight.deploy();
-    console.log('3')
     await flight.waitForDeployment();
-    console.log('4')
+    
     const deploymentAddressFlight = await flight.getAddress();
 
     console.log("Flight deployed at", `${deploymentAddressFlight}`)
